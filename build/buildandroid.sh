af44b88fa915933e1effbffd6520e6edbc666bb9
mycral <guoliulong@gmail.com>
2018-02-09 13:12:15 +0800

    Update buildandroid.sh

--- build/buildandroid.sh
+++ build/buildandroid.sh
@@ -1,6 +1,6 @@
 CMAKE_PATH=./../cmake/3.6.3155560/bin/
 NDK_PATH=/c/android-ndk-r14b
-
+#NDK_PATH=c:/android-ndk-r14b
 ${CMAKE_PATH}/cmake .. -DCMAKE_BUILD_TYPE=Debug -DANDROID_PIE=TRUE -DANDROID_ABI="armeabi-v7a with NEON" -DCMAKE_TOOLCHAIN_FILE=${NDK_PATH}/build/cmake/android.toolchain.cmake -DANDROID=TRUE -G"Unix Makefiles"
 
 make
